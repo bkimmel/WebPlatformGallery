9247586151a4fbc2c0bb3cb63dc5d745fb243cff
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-04-30 12:27:50 -0400

    .

--- JS_Node/debuggingnode.js
+++ JS_Node/debuggingnode.js
@@ -1,5 +1,16 @@
 //WITH REPL:
-
+  //run node debug program.js
+  //this will being up the debug> prompt
+    //debug> commands:
+	  // repl :: drop into repl to inspect vars, objects, etc.
+	  // c/cont :: continue to next breakpoint
+	  // n/next :: go to next line
+	  // s/step :: step into function call
+	  // o/out :: step out of function call
+	  // sb :: set breakpoint
+	  // backtrace :: print call stack
+	  
+var a = 0;
 function toplev() {
 	var a = 1;
 	function midlev() {
