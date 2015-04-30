8dd728cd3069ced5915483b15c73c1b795a81809
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-04-30 13:00:56 -0400

    .

--- JS_Node/debuggingnode.js
+++ JS_Node/debuggingnode.js
@@ -9,7 +9,19 @@
 	  // o/out :: step out of function call
 	  // sb :: set breakpoint
 	  // backtrace :: print call stack
-	  
+
+//WITH NODE-INSPECTOR:
+  //run node --debug program.js
+  //in a seperate window run node-inspector
+    //go to the URL it serves from and it will open a devtools for you
+
+var http = require('http');
+var connect = require('connect');
+var events = require('events');
+var url = require('url');
+var fs = require('fs');
+var lo_ = require('lodash');
+
 var a = 0;
 function toplev() {
 	var a = 1;
@@ -18,6 +30,19 @@ function toplev() {
 		function lowlev() {
 			console.log('entering debugger');
 			debugger;
+			return a;
 		}
+		return lowlev();
 	}
+	return midlev();
 }
+
+console.log( toplev() );
+
+var server = connect()
+ .use(function(req, res, next) {
+	res.write( 'result: ' + toplev() );
+ });
+ 
+server.listen(3000);
+
