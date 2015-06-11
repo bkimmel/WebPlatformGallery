4e2b22d911414ba3277fadaa1ec8db894a07ab2c
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-06-11 14:17:54 -0400

    semaphore example

--- JS_Primitive/Iterables.js
+++ JS_Primitive/Iterables.js
@@ -1,3 +1,64 @@
+//Use the Symbol.iterator key to get the iterator for an 'iterable' thing.
+var j = [1,2,3,4,5];
+var iter = j[Symbol.iterator]();
+//Use .next to iterate over it:
+iter.next() //=> {value: 1, done: false}
+//Use 'of' operator to iterate:
+for (t of iter) { console.log(t); }
+//=> 2
+//=> 3
+//=> 4
+//=> 5
+//=> undefined
+
+var customiter = {
+};
+//At the meta-key for Symbol.iterator, you add the 'iterator function' which returns a module
+//with the 'next' property that consumers will use.
+customiter[Symbol.iterator] = function() {
+	var count = 10;
+	return {
+		'next': function() {
+			return count > 0 ? {value: count--, done: false} : {value: undefined, done: true}
+		}
+	}
+}
+//the for/of construct automatically unpacks the value from each iteration for you
+for (x of customiter) {console.log(x)}
+//=> 10
+//=> 9
+//=> ...
+//=> 1
+
+//A more involved example
+var asynciter = {
+};
+asynciter[Symbol.iterator] = function() {
+	var count = 10;
+	return {
+		'next': function() {
+			return count > 0 ? {value: new Promise(function(res,rej){
+				setTimeout(function(){
+					res(count--);
+				},Math.random() * 1000);
+			}), done: false} : {value: undefined, done: true}
+		}
+	}
+}
+function drain(iter) {
+	var v = iter.next();
+	if (!!v.done) {
+		console.log('%cDrain Finished', 'color:red;');
+		console.groupEnd();
+		return true;
+	}
+	v.value.then(function(v){ console.log(v); drain(iter); });
+}
+console.group('Drain Started...');
+drain( asynciter[Symbol.iterator]() );
+//=> 10
+
+
 //A somewhat simple semaphore implemented with iterators 
 function sem() {
 	if(this.arr.length) {
@@ -68,3 +129,15 @@ function lineup(t, f) {
 lineup(thing, function(v){ console.log('%i', this.num, ' received:%i', v); }.bind({num: 4}));
 lineup(thing, function(v){ console.log('%i', this.num, ' received:%i', v); }.bind({num: 5}));
 lineup(slowpoke.next().value, function(v){ console.log('Slowpoke received:%i', v); })
+
+
+
+
+
+
+
+
+
+
+
+
