d300a34e062c0f6c58f5aa04daee8eb065edc284
Brent Kimmel <brentkimmel@brentkimmel.com>
2015-07-07 14:59:01 -0400

    performance api stuff

---
+++ JS_Dom/performanceAPI.js
@@ -0,0 +1,27 @@
+//This creates a point from which performance can be measured.
+window.performance.mark('mark1');
+//This creates another one
+setTimeout(function(){
+	window.performance.mark('mark2');
+	//This creates a 'measure' entry with a duration from mark1 to mark2, with a name of test
+	window.performance.measure('test','mark1','mark2');
+	
+	//This gets the time from mark1 to mark2 (should be roughly 3 seconds)
+	console.log('mark1 -> mark2: %f', performance.getEntriesByName('test')[0].duration );
+}, 3000);
+
+//You can also use named events from the Performance model, like 'domInteractive':
+window.performance.measure('test2','domInteractive','mark1');
+console.log('domInteractive -> mark1: %f', performance.getEntriesByName('test2')[0].duration );
+
+//Get individual resource times...e.g. here we look for a big image named hero_k12home_load, and we
+//fetch a bigger image only if it's load time was fast enough.
+var perf = window.performance.getEntries();
+var imgentry = perf.filter(function(v){ return v.name.match(/hero_k12home_load/) })[0];
+if(!imgentry || !imgentry.duration || imgentry.duration > 1200) {
+  //too slow, don't load the big one
+  console.log('cancelled image load');
+  return false;
+}
+
+
