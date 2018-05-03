a54a9bb4fa8ce8afb42e06a7f0b6a3b1d6c7925d
Brent Kimmel <bkimmel@users.noreply.github.com>
2018-05-03 14:02:29 -0400

    Update ArrowFunctionsThisBinding.js

--- JS_Primitive/ArrowFunctionsThisBinding.js
+++ JS_Primitive/ArrowFunctionsThisBinding.js
@@ -24,3 +24,25 @@ function c(f) {
 var d = () => { console.log(this); }
 c(d); //window
 c.bind({def: 456})(d); //window <-- still from where it was declared
+
+var foo = {
+  bar: 123,
+  f: ()=> { 
+    console.log(this.bar) //(A)
+  }, 
+  g: {
+    bar: 456,
+    h: ()=>{ console.log(this.bar) }
+  },
+  i: () => {
+    let bar = 678;
+    return ()=>{ console.log(this.bar) };
+  },
+  j: function(){
+    return ()=>{ console.log(this.bar) }
+  }
+}
+foo.f(); //undefined, because at (A) the lexical scope is 'window' (foo is not a function and object literals do not create scope)
+foo.g.h(); //also undefined, same reason as above - no matter how many "layers" of object literal you create
+foo.i()(); //also undefined, same as above -> wrapping arrow functions in arrow functions also does not create lexical scope
+foo.j()(); //123 : a normal function *does* create lexical scope (in this case, the object "foo" of which it is a method) and this is what the arrow function uses
